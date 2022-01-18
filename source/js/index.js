8280dd7218da40988a336fd2d9ff879036cde34b
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-01-18 15:59:37 +0300

    Работа над базовой стилизацией для мобильной версии

---
+++ source/js/index.js
@@ -0,0 +1,25 @@
+const topMenu = document.querySelector(".top-menu");
+const burgerBtn = document.querySelector(".main-nav__toggle");
+const alphabetBtns = document.querySelectorAll(".alphabet__button");
+
+topMenu.classList.add("top-menu--close")
+
+burgerBtn.addEventListener("click", () => {
+  topMenu.classList.toggle("top-menu--open");
+  topMenu.classList.toggle("top-menu--close");
+})
+
+alphabetBtns.forEach(el => el.addEventListener("click", () => {
+  document.querySelector(".alphabet__button--active").classList.remove("alphabet__button--active");
+  el.classList.toggle("alphabet__button--active");
+}))
+
+document.querySelector(".country-filters__button").addEventListener("click", () => {
+  if (document.querySelector(".country-filters").classList.contains("country-filters--close")) {
+    document.querySelector(".country-filters").classList.remove("country-filters--close")
+    document.querySelector(".country-filters").classList.add("country-filters--open")
+  } else {
+    document.querySelector(".country-filters").classList.remove("country-filters--open")
+    document.querySelector(".country-filters").classList.add("country-filters--close")
+  }
+})

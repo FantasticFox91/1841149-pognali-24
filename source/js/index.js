6bcb2677073e717a23025e5a165ebe4a8e923a2a
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-01-28 17:00:41 +0300

    Работа над стилизацией интерактивных элементов

--- source/js/index.js
+++ source/js/index.js
@@ -2,6 +2,9 @@ const topMenu = document.querySelector(".top-menu");
 const burgerBtn = document.querySelector(".main-nav__toggle");
 const alphabetBtns = document.querySelectorAll(".alphabet__button");
 const dropdownBtn = document.querySelector(".country__button--dropdown");
+const countryFiltersBtn = document.querySelector(".country-filters__button");
+const modalBtn = document.querySelector(".add-profile__link");
+const modal = document.querySelector(".modal")
 
 topMenu.classList.add("top-menu--close")
 
@@ -15,14 +18,21 @@ alphabetBtns.forEach(el => el.addEventListener("click", () => {
   el.classList.toggle("alphabet__button--active");
 }))
 
-document.querySelector(".country-filters__button").addEventListener("click", () => {
-  if (document.querySelector(".country-filters").classList.contains("country-filters--close")) {
-    document.querySelector(".country-filters").classList.remove("country-filters--close")
-    document.querySelector(".country-filters").classList.add("country-filters--open")
-  } else {
-    document.querySelector(".country-filters").classList.remove("country-filters--open")
-    document.querySelector(".country-filters").classList.add("country-filters--close")
-  }
-})
 
-dropdownBtn.onclick = () => dropdownBtn.classList.toggle("country__button--active");
+countryFiltersBtn !== null ? countryFiltersBtn.addEventListener("click", () => {
+    if (document.querySelector(".country-filters").classList.contains("country-filters--close")) {
+      document.querySelector(".country-filters").classList.remove("country-filters--close")
+      document.querySelector(".country-filters").classList.add("country-filters--open")
+    } else {
+      document.querySelector(".country-filters").classList.remove("country-filters--open")
+      document.querySelector(".country-filters").classList.add("country-filters--close")
+    }
+  })
+  : null;
+
+dropdownBtn !== null ? dropdownBtn.addEventListener("click", () => dropdownBtn.classList.toggle("country__button--active")) : null;
+
+modalBtn !== null ? modalBtn.addEventListener("click", (event) => {
+  event.preventDefault()
+  modal.classList.toggle("modal--open")})
+  : null;
